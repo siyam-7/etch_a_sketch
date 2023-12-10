088716e2c0d0ed2ed4031053e688b4c9cc6202e3
siyam <siyam@pop-os.localdomain>
2023-12-11 00:02:15 +0530

    adds the hovering effect on the grid

--- main.js
+++ main.js
@@ -6,3 +6,16 @@ for(i=0;i<16*16;i++){
 
     container.append(newDiv);
 }
+
+
+ const divs =document.getElementsByClassName("grid_div");
+ console.log(divs.length);
+    Array.from(divs).forEach(function (div){
+        div.addEventListener('mouseenter', function () {
+            div.style.backgroundColor = "red";
+        });
+        div.addEventListener('mouseleave', function(){
+            div.style.backgroundColor = "white";
+        })
+    });
+    
