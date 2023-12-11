f0bd9ffd5c0f890a340b6939cf61ea9efa6f2460
siyam <siyam@pop-os.localdomain>
2023-12-11 13:57:24 +0530

    adds the random color effect

--- main.js
+++ main.js
@@ -10,6 +10,10 @@ let grid_size=16;
 adjust.addEventListener('click', function(){
     grid_size = prompt("Please enter the grid size");
     grid_size = parseInt(grid_size);
+    if(grid_size>100){
+        alert("Maximum size is 100! Try Again.")
+        return;
+    }
     container.innerHTML ="";
     const grid_hw = grid_size*50;
     container.style.height=grid_hw + "px";
@@ -24,7 +28,8 @@ adjust.addEventListener('click', function(){
  console.log(divs.length);
     Array.from(divs).forEach(function (div){
         div.addEventListener('mouseenter', function () {
-            div.style.backgroundColor = "red";
+            const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+            div.style.backgroundColor = randomColor;
         });
         div.addEventListener('mouseleave', function(){
             div.style.backgroundColor = "white";
@@ -40,12 +45,13 @@ for(i=0;i<grid_size*grid_size;i++){
     container.append(newDiv);
 }
 
-
+ 
  const divs =document.getElementsByClassName("grid_div");
  console.log(divs.length);
     Array.from(divs).forEach(function (div){
         div.addEventListener('mouseenter', function () {
-            div.style.backgroundColor = "red";
+          const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+          div.style.backgroundColor = randomColor;
         });
         div.addEventListener('mouseleave', function(){
             div.style.backgroundColor = "white";
