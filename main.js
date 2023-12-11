4abdc81e4ffeb7e2feae84232cfe9739e9527604
siyam <siyam@pop-os.localdomain>
2023-12-11 13:40:26 +0530

    adds the adjust button

--- main.js
+++ main.js
@@ -1,6 +1,39 @@
-const container = document.querySelector('.container');
+const mastercontainer = document.querySelector('.master-container');
+const adjust = document.createElement("button");
+adjust.className="adjust_button";
+adjust.innerHTML ="ADJUST THE GRID SIZE";
+mastercontainer.insertBefore(adjust,mastercontainer.firstChild);
 
-for(i=0;i<16*16;i++){
+const container = document.querySelector('.container');
+let grid_size=16;
+
+adjust.addEventListener('click', function(){
+    grid_size = prompt("Please enter the grid size");
+    grid_size = parseInt(grid_size);
+    container.innerHTML ="";
+    const grid_hw = grid_size*50;
+    container.style.height=grid_hw + "px";
+    container.style.width = grid_hw +"px";
+    for(i=0;i<grid_size*grid_size;i++){
+        var newDiv = document.createElement('div');
+        newDiv.className="grid_div";
+    
+        container.append(newDiv);
+    }
+    const divs =document.getElementsByClassName("grid_div");
+ console.log(divs.length);
+    Array.from(divs).forEach(function (div){
+        div.addEventListener('mouseenter', function () {
+            div.style.backgroundColor = "red";
+        });
+        div.addEventListener('mouseleave', function(){
+            div.style.backgroundColor = "white";
+        })
+    });
+  
+})    
+
+for(i=0;i<grid_size*grid_size;i++){
     var newDiv = document.createElement('div');
     newDiv.className="grid_div";
 
@@ -18,4 +51,4 @@ for(i=0;i<16*16;i++){
             div.style.backgroundColor = "white";
         })
     });
-    
+  
